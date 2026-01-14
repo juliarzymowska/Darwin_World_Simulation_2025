9f818a508d9b433fed450e021c7b98c4c6f2e37a
Julia Rzymowska <jrzymowska@student.agh.edu.pl>
2026-01-14 14:35:18 +0100

    update Simulation add random placement for animals
    (TODO: move random to other class)

--- project/src/main/java/agh/ics/oop/Simulation.java
+++ project/src/main/java/agh/ics/oop/Simulation.java
@@ -1,5 +1,7 @@
 package agh.ics.oop;
 
+import agh.ics.oop.configuration.ConfigAnimal;
+import agh.ics.oop.configuration.ConfigMap;
 import agh.ics.oop.model.elements.Animal;
 import agh.ics.oop.model.util.Vector2d;
 import agh.ics.oop.model.map.WorldMap;
@@ -8,19 +10,28 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Simulation implements Runnable {
-    private final List<Animal> animals = new ArrayList<>();
+    // Lists to keep track of alive and dead animals for keeping statistics
+    private final List<Animal> aliveAnimals = new ArrayList<>();
+    private final List<Animal> deadAnimals = new ArrayList<>();
     private final WorldMap map;
-    // some error for config file that doesn't let user make a genotypeLength = 0!
+
+    // some error for config file that doesn't let user make a genotypeLength = 0m!
     // error for config file when number of grass per day is bigger than map tiles number
-    // random animal position generation
+
+    // (for tests) Constructor of Simulation class.
     public Simulation(List<Vector2d> positions, WorldMap map) {
         this.map = map;
 
         for (Vector2d pos : positions) {
             Animal newAnimal = new Animal(pos);
             map.placeAnimal(newAnimal);
-            animals.add(newAnimal);
+            aliveAnimals.add(newAnimal);
         }
+    }
+
+    public Simulation(ConfigAnimal configAnimal, ConfigMap configMap, WorldMap map) {
+        this.map = map;
+        generateAnimalsOnMap(configAnimal, configMap, map);
     }
 
     public void run() {
@@ -31,13 +42,13 @@ public class Simulation implements Runnable {
             // 1. Remove dead animals
             removeDeadAnimals(currentDay);
 
-            if (animals.isEmpty()) {
+            if (aliveAnimals.isEmpty()) {
                 IO.println("All animals have died. Simulation ending.");
                 break;
             }
 
             // 2. Move animals
-            for (Animal animal : animals) {
+            for (Animal animal : aliveAnimals) {
                 map.moveTo(animal);
             }
 
@@ -55,20 +66,39 @@ public class Simulation implements Runnable {
         }
     }
 
+    private void generateAnimalsOnMap(ConfigAnimal configAnimal, ConfigMap configMap, WorldMap map) {
+        for (int i = 0; i < configAnimal.initialAnimalCount(); i++) {
+            // Random position generation, could lead to infinite loop if map is full
+            // Can place multiple animals on the same position
+            Vector2d position = new Vector2d(
+                    (int) (Math.random() * configMap.width()),
+                    (int) (Math.random() * configMap.height())
+            );
+            Animal newAnimal = new Animal(position);
+            try {
+                map.placeAnimal(newAnimal);
+                aliveAnimals.add(newAnimal);
+            } catch (Exception e) {
+                System.err.println("Failed to place animal: " + e.getMessage());
+            }
+        }
+    }
+
     private void removeDeadAnimals(int currentDay) {
-        List<Animal> deadAnimals = animals.stream()
+        List<Animal> currentDayDeadAnimals = aliveAnimals.stream()
                 .filter(animal -> !animal.isAlive())
                 .toList();
 
-        for (Animal animal : deadAnimals) {
+        for (Animal animal : currentDayDeadAnimals) {
             animal.die(currentDay);
             map.removeAnimal(animal);
         }
 
-        animals.removeAll(deadAnimals);
+        aliveAnimals.removeAll(currentDayDeadAnimals);
+        deadAnimals.addAll(currentDayDeadAnimals);
     }
 
     public List<Animal> getAnimals() {
-        return List.copyOf(animals);
+        return List.copyOf(aliveAnimals);
     }
 }
